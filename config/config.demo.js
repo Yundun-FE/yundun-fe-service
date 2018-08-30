9208e35cc4ffea5bb730da47d80b92a7e1fcc7e8
kongfanbo@yundun.com <xank@qq.com>
2018-08-30 11:32:30 +0800

    conf

--- config/config.demo.js
+++ config/config.demo.js
@@ -6,8 +6,24 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1535530696448_9519'
 
+  config.sequelize = {
+    dialect: 'mysql',
+    database: 'yundun-fe',
+    host: 'localhost',
+    port: '3306',
+    username: 'root',
+    password: 'root',
+    timezone: '+08:00'
+  }
+
   // add your config here
   config.middleware = []
+
+  config.security = {
+    csrf: {
+      enable: false,
+    }
+  }
 
   return config
 }
