b8ebc3a2dc99ac79bbec0a3e54cf5ba65684332c
xank <xank@qq.com>
2019-01-31 22:30:18 +0800

    rm

--- app/service/applicationsPages.js
+++ app/service/applicationsPages.js
@@ -6,19 +6,9 @@ const { isDef } = require('../utils');
 class applicationsPagesService extends Service {
   async getByCode(code, agent) {
     const data = await this.ctx.model.ApplicationsPages.findOne({ where: { code } });
-    const words = {};
-    data.words.forEach(item => {
-      words[item.key] = agent && isDef(item.valueOem) ? item.valueOem : item.value;
-    });
-    const settings = {};
-    data.settings.forEach(item => {
-      settings[item.key] = agent && isDef(item.valueOem) ? item.valueOem : item.value;
-    });
 
     return {
       name: data.name,
-      words,
-      settings,
       content: data.content,
     };
   }
